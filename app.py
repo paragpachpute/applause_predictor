672cc9cba28659cd600b5e5b93ce29f7f190dd23
Chirag Goel <chirag@Chirags-MacBook-Pro.local>
2020-06-22 00:04:15 -0400

    cleaned

--- app.py
+++ app.py
@@ -174,12 +174,5 @@ def detect():
 	return {"confidence": x.tolist(), "pred" : predictions[0][2]}
 
 
-    
-
-@app.route('/hello')
-def hello():
-    return "Hello, World!"
-
-
 if __name__ == '__main__':
     app.run(debug=True)
